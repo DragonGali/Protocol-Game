070885a5217942fea8a2aa6fcfe1db10caa2c118
Gali Kertser <gali.kertser@gmail.com>
2025-09-03 15:21:23 +0300

    I need to make the monitor scalable

--- ProtocolGame/src/Components/GameScreen.jsx
+++ ProtocolGame/src/Components/GameScreen.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import '../Styles/GameScreen.css';
 import CustomerView from './CustomerView.jsx';
+import Monitor from './Monitor.jsx';
 
 function GameScreen() {
 
   return (
       <div className="GameScreen">
           <CustomerView className="CustomerView"/>
+          <Monitor className="Monitor"/>
       </div>
   );
 }
---
+++ ProtocolGame/src/Components/Monitor.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import '../Styles/Monitor.css';
+
+function Monitor() {
+  return (
+    <div className="Monitor">
+    </div>
+  );
+}
+
+export default Monitor;
